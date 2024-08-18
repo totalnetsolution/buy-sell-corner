ec99916e1192450e7317d61a545786cfb413c4bf
muhammad-danish <totalnetsolution2@gmail.com>
2024-08-19 02:10:46 +0500

    index file modified

--- firebase-config.js
+++ firebase-config.js
@@ -17,10 +17,6 @@ export const auth = getAuth();
 export const db = getFirestore();
 export const storage = getStorage();
 
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyDugzyIqoy_AqN_RzsaTn-CIoQ9cs-DffA",
   authDomain: "buy-sell-corner-13c1f.firebaseapp.com",
@@ -30,8 +26,7 @@ const firebaseConfig = {
   appId: "1:69339340088:web:2fa1a0d2c8ad125ea2b5cd"
 };
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
+export const app = initializeApp(firebaseConfig);
 export const firebaseapp = firebase.initializeApp(firebaseConfig);
 export const Auth = getAuth();
 export const Firestore = getFirestore();
